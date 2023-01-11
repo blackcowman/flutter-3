fcf270db085010b57f03ccd6b79d3703f6d7fecd
blackcowman <soh990301@gmail.com>
2023-01-11 11:36:23 +0900

    main changes

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,39 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("hello flutter"),
+        backgroundColor: Color(0xFF181818),
+
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            children: [
+              SizedBox(
+                height:80,
+              ),
+              Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text('Hey, Selena ',
+                    style: TextStyle(color: Colors.white,
+                    fontSize: 28,
+                    fontWeight: FontWeight.w800,
+                    ),
+                    ),
+                    Text('Welcome back',
+                    style: TextStyle(color: Colors.white.withOpacity(0.7),
+                    fontSize: 18,
+                    ),
+                    ),
+                  ],
+                ),
+              ],
+
+            ),],
+          ),
         ),
-        body: Text("hello world"),
       ),
     );
   }
